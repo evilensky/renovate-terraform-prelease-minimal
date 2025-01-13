9c8ca669a4803e561c9cea44a190600861d2d210
Eugene Vilensky <eugene.vilensky@cambiahealth.com>
2025-01-13 10:44:08 -0600

    use self-tagged module instead

--- main.tf
+++ main.tf
@@ -1,27 +1,4 @@
-module "cloudflare" {
-  source  = "cloudflare/cloudflare"
-  version = "4.0.0"
-
-  zone_id = cloudflare_zone.domain.id
-
-  settings = {
-    # SSL/TLS Settings
-    ssl = "strict"
-
-    # Security Settings
-    security_level        = "medium"
-    challenge_ttl        = 1800
-    browser_check       = "on"
-
-    # Performance Settings
-    minify = {
-      css  = "on"
-      html = "on"
-      js   = "on"
-    }
-
-    # Cache Settings
-    browser_cache_ttl = 14400
-    always_online    = "on"
-  }
+module "monkeysnot" {
+  source  = "evilensky/monkeysnot/aws"
+  version = "0.0.1"
 }
